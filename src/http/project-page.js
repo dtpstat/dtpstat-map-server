e2cee821f943dfa2e1d27f702c3545e804f1716d
Kulikov Aleksei <k.alex.serg@gmail.com>
2026-09-05 22:45:31 +0200

    Render analytics configuration from project settings

--- src/http/project-page.js
+++ src/http/project-page.js
@@ -7,13 +7,42 @@ function escapeHtml(value) {
     .replaceAll("'", '&#39;');
 }
 
+function metricsMarkup(settings) {
+  const yandexMetrikaId = settings.yandexMetrikaId
+    ? escapeHtml(settings.yandexMetrikaId)
+    : '';
+  const googleAnalyticsId = settings.googleAnalyticsId
+    ? escapeHtml(settings.googleAnalyticsId)
+    : '';
+  const meta = [
+    yandexMetrikaId
+      ? `<meta name="yandex-metrika-id" content="${yandexMetrikaId}">`
+      : '',
+    googleAnalyticsId
+      ? `<meta name="google-analytics-id" content="${googleAnalyticsId}">`
+      : '',
+  ].filter(Boolean).join('\n    ');
+  const script = yandexMetrikaId || googleAnalyticsId
+    ? '<script type="module" src="/js/metrics.js"></script>'
+    : '';
+  const yandexNoScript = yandexMetrikaId
+    ? `<img src="https://mc.yandex.ru/watch/${yandexMetrikaId}" alt="" hidden>`
+    : '';
+
+  return { meta, script, yandexNoScript };
+}
+
 export function renderProjectPage(template, settings) {
   const projectName = escapeHtml(settings.projectName);
   const keywords = escapeHtml((settings.keywords ?? []).join(', '));
+  const metrics = metricsMarkup(settings);
   return template
     .replaceAll('{{PROJECT_NAME}}', projectName)
     .replaceAll('{{PROJECT_KEYWORDS}}', keywords)
-    .replace('{{PROJECT_FOOTER_HTML}}', settings.footerHtml);
+    .replace('{{PROJECT_METRICS_META}}', metrics.meta)
+    .replace('{{PROJECT_FOOTER_HTML}}', settings.footerHtml)
+    .replace('{{YANDEX_METRIKA_NOSCRIPT}}', metrics.yandexNoScript)
+    .replace('{{PROJECT_METRICS_SCRIPT}}', metrics.script);
 }
 
 export function projectManifest(settings) {
