8d9b9c3f58ce91876a3f852293a4812b374efcb9
Kulikov Aleksei <k.alex.serg@gmail.com>
2026-09-06 01:39:37 +0200

    Use configured schema for maintenance connections

--- scripts/database.js
+++ scripts/database.js
@@ -1,12 +1,18 @@
 import pg from 'pg';
-import { loadApplicationDatabaseConnection } from '../src/db/database-environment.js';
+import {
+  databaseApplicationName,
+  databaseSearchPath,
+  loadApplicationDatabaseConnection,
+  loadDatabaseSchema,
+} from '../src/db/database-environment.js';
 
 const { Client } = pg;
 
 export function createDatabaseClient() {
+  const schema = loadDatabaseSchema();
   return new Client({
     ...loadApplicationDatabaseConnection(),
-    application_name: 'dtpstat-buslines-maintenance',
-    options: '-c search_path=buslanes,public',
+    application_name: databaseApplicationName(schema, 'maintenance'),
+    options: databaseSearchPath(schema),
   });
 }
